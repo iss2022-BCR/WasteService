23add1bb28965120a0a9ba309af87d6dad43e88d
Michele Righi <righi.michy@gmail.com>
2022-11-28 03:30:44 +0100

    update docs
    Aggiunti Sprint Review e Sprint Retrospective - da finire (domani faccio)

---
+++ Sprint1/Sprint1_Project/run_WasteService.bat
@@ -0,0 +1,10 @@
+cd sprint1_waste_service
+
+:: Run WasteService Context
+start .\gradlew runCtx_WasteService
+
+:: Run VirtualRobot Context
+start .\gradlew runCtx_Robot
+
+:: Run TransportTrolley Context
+start .\gradlew runCtx_TransportTrolley
---
+++ Sprint1/Sprint1_Project/scripts/run_Echo_Waste_Service.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cd ../sprint1_echo_waste_server
+flutter run
