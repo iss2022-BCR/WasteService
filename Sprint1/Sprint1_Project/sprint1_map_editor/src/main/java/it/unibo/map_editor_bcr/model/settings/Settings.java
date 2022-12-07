83f1f598dc28bd10723ee299c04c8b210ba1cfbc
Michele Righi <righi.michy@gmail.com>
2022-12-07 05:19:56 +0100

    update MapEditor v1.2
    Piccolo refactor funzionalità:
    + le entry delle impostazioni sono state riordinate;
    + la logica delle entry dei file delle mappe è stata cambiata: ora mostrano il file corrente utilizzato, e solo quella della RoomMap è modificabile, in quanto l'altra si può già caricare/salvare dall'editor;

--- Sprint1/Sprint1_Project/sprint1_map_editor/src/main/java/it/unibo/map_editor_bcr/model/settings/Settings.java
+++ Sprint1/Sprint1_Project/sprint1_map_editor/src/main/java/it/unibo/map_editor_bcr/model/settings/Settings.java
@@ -1,11 +1,11 @@
 package it.unibo.map_editor_bcr.model.settings;
 
 public class Settings {
-    private boolean darkTheme;
-    private final LogLevel logLevel = new LogLevel();
     private String roomMapFile;
     private String mapConfigFile;
-    private boolean confirmBeforeSaving;
+    private final LogLevel logLevel = new LogLevel();
+    private boolean confirmations;
+    private boolean darkTheme;
     private String coordinateColor;
 
     public Settings() {
@@ -15,34 +15,36 @@ public class Settings {
         this.logLevel.directory = Constants.DEFAULT_LOG_LEVEL_DIRECTORY;
         this.roomMapFile = Constants.DEFAULT_ROOM_MAP_FILE;
         this.mapConfigFile = Constants.DEFAULT_MAP_CONFIG_FILE;
-        this.confirmBeforeSaving = Constants.DEFAULT_CONFIRM_BEFORE_SAVING;
+        this.confirmations = Constants.DEFAULT_CONFIRM_BEFORE_SAVING;
         this.coordinateColor = Constants.DEFAULT_COORDINATE_COLOR;
     }
-    public Settings(boolean darkTheme, boolean logLevelToStandardOutput, boolean logLevelToFile, String logLevelDirectory,
-                    String loadRoomMapFilename, String loadMapConfigFilename, String coordinateColor) {
-        this.darkTheme = darkTheme;
+    public Settings(String roomMapFile, String mapConfigFile,
+                    boolean logLevelToStandardOutput, boolean logLevelToFile, String logLevelDirectory,
+                    boolean confirmations, boolean darkTheme, String coordinateColor) {
+        this.roomMapFile = roomMapFile;
+        this.mapConfigFile = mapConfigFile;
         this.logLevel.toStandardOutput = logLevelToStandardOutput;
         this.logLevel.toFile = logLevelToFile;
         this.logLevel.directory = logLevelDirectory;
-        this.roomMapFile = loadRoomMapFilename;
-        this.mapConfigFile = loadMapConfigFilename;
+        this.confirmations = confirmations;
+        this.darkTheme = darkTheme;
         this.coordinateColor = coordinateColor;
     }
 
-    public boolean isDarkTheme() {return darkTheme;}
-    public void setDarkTheme(boolean darkTheme) {this.darkTheme = darkTheme;}
+    public String getRoomMapFile() {return roomMapFile;}
+    public void setRoomMapFile(String roomMapFile) {this.roomMapFile = roomMapFile;}
+    public String getMapConfigFile() {return mapConfigFile;}
+    public void setMapConfigFile(String mapConfigFile) {this.mapConfigFile = mapConfigFile;}
     public LogLevel getLogLevel() {return logLevel;}
     public void setLogLevel(boolean toStdOutput, boolean toFile, String directory) {
         logLevel.toStandardOutput = toStdOutput;
         logLevel.toFile = toFile;
         logLevel.directory = directory;
     }
-    public String getRoomMapFile() {return roomMapFile;}
-    public void setRoomMapFile(String roomMapFile) {this.roomMapFile = roomMapFile;}
-    public String getMapConfigFile() {return mapConfigFile;}
-    public void setMapConfigFile(String mapConfigFile) {this.mapConfigFile = mapConfigFile;}
-    public boolean isConfirmBeforeSaving() {return confirmBeforeSaving;}
-    public void setConfirmBeforeSaving(boolean confirmBeforeSaving) {this.confirmBeforeSaving = confirmBeforeSaving;}
+    public boolean isConfirmations() {return this.confirmations;}
+    public void setConfirmations(boolean confirmations) {this.confirmations = confirmations;}
+    public boolean isDarkTheme() {return darkTheme;}
+    public void setDarkTheme(boolean darkTheme) {this.darkTheme = darkTheme;}
     public String getCoordinateColor() {return coordinateColor;}
     public void setCoordinateColor(String coordinateColor) {this.coordinateColor = coordinateColor;}
 }
