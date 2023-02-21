fa3b8ad8b61d82708cba4722142394a0ff9029a5
Michele Righi <righi.michy@gmail.com>
2023-02-22 00:27:14 +0100

    test led state QAK

--- Sprint2/Sprint2_ProblemAnalysis/test_led_state.pl
+++ Sprint2/Sprint2_ProblemAnalysis/test_led_state.pl
@@ -4,5 +4,6 @@
 context(ctx_ledstate_test, "localhost",  "TCP", "11705").
  qactor( transporttrolley_ls_test, ctx_ledstate_test, "it.unibo.transporttrolley_ls_test.Transporttrolley_ls_test").
   qactor( pathexecutorbcr_ls_test, ctx_ledstate_test, "it.unibo.pathexecutorbcr_ls_test.Pathexecutorbcr_ls_test").
+  qactor( trolleystateprovider_ls_test, ctx_ledstate_test, "it.unibo.trolleystateprovider_ls_test.Trolleystateprovider_ls_test").
   qactor( alarmcontroller_ls_test, ctx_ledstate_test, "it.unibo.alarmcontroller_ls_test.Alarmcontroller_ls_test").
   qactor( ledcontroller_ls_test, ctx_ledstate_test, "it.unibo.ledcontroller_ls_test.Ledcontroller_ls_test").
