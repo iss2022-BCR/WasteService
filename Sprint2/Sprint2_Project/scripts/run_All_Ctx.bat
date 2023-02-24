cdb58e04e3e18f3983d3dabf113990ae9dd437ce
Michele Righi <righi.michy@gmail.com>
2023-02-24 02:30:56 +0100

    sprint2 project creation

---
+++ Sprint2/Sprint2_Project/scripts/run_All_Ctx.bat
@@ -0,0 +1,16 @@
+cd ../sprint2_waste_service/
+
+:: Run SmartDevice Context
+:: start .\gradlew runCtx_SmartDevice
+
+:: Run Raspberry Context
+start .\gradlew runCtx_RaspberryPi
+
+:: Run Robot Context
+start .\gradlew runCtx_Robot
+
+:: Run TransportTrolley Context
+start .\gradlew runCtx_TransportTrolley
+
+:: Run WasteService Context
+start .\gradlew runCtx_WasteService
---
+++ Sprint2/Sprint2_Project/scripts/run_AndroidEmulator.bat
@@ -0,0 +1,2 @@
+:: run Android Emulator
+flutter emulators --launch Pixel_3a_API_30_x86
