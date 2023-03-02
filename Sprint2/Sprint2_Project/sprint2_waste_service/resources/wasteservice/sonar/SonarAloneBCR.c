1c0b5710d3aea043c6bea31e2d73f23a666f0014
Michele Righi <righi.michy@gmail.com>
2023-03-02 02:14:49 +0100

    Update SonarAloneBCR.c

--- Sprint2/Sprint2_Project/sprint2_waste_service/resources/wasteservice/sonar/SonarAloneBCR.c
+++ Sprint2/Sprint2_Project/sprint2_waste_service/resources/wasteservice/sonar/SonarAloneBCR.c
@@ -1,34 +1,61 @@
 /**
 * Sonar Alone by team BCR
-* 
+*
 * Build with: gcc SonarAloneBCR.c -o SonarAloneBCR -lwiringPi
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 #include <wiringPi.h>
 
 #define GPIO_BUZZER 1	// 18
 #define GPIO_TRIGGER 4	// 23
 #define GPIO_ECHO 5	// 24
 
-void printUsage(int argc, char** argv);
-void parseParameters();
+enum Flags
+{
+	FLAG_IP,	// -i/--ip <ip_address>
+	FLAG_PORT,	// -p/--port <port> || -d/--direction <direction>
+	FLAG_RAND,	// -r/--random
+	FLAG_DIST,	// -d/--distance <distance>
+	FLAG_ERR,
+	FLAG_MAX,
+};
+
+void printUsage();
+void parseParameters(int argc, char** argv);
 int init();
 float distanceCM();
+void doBuzz(int delay);
+
+void cleanup(int);
+void setOnQuit(void (*function)(int));
 
 int main(int argc, char** argv)
 {
         init();
         // parse arguments
+        setOnQuit(cleanup);
         
         for(;;)
         {
                 printf("%d\n", (int) distanceCM());
+                
+                doBuzz(25);
+                
+                
                 delay(500);
         }
         
         return 0;
+}
+
+void printUsage(char* cmd)
+{
+	printf("USAGE:\n\t%s [-b/--buzzer] [-d/--delay <delay-value>] [--unit-of-measure <m | dm | cm | mm>] [--lower-bound <lower-bound>] [--upper-bound <upper-bound>] [-r/--random]\n\n\n", cmd);
+	//printf("-i/--ip <ip_address>, speficies the IP address of the remote host on which send the packets (default is %s)\n\n-p/--port <port>, specifies the remote port on which send the packets (default is %d)\n\n-r/--random, specifies that the distance will be generated with a RNG\n\n-D/--distance <distance>, specifies the distance lower limit under which the led will light up (default is %3.1f)\n\n", argv[0], DEFAULT_IP, DEFAULT_SND_PORT, DEFAULT_DLIM);
 }
 
 
@@ -76,4 +103,29 @@ float distanceCM()
         
         // multiply with the sonic speed (34300 cm/s) and divide by 2
         return (timeElapsed * 34300) / 2;
+}
+void doBuzz(int delayAmount)
+{
+        digitalWrite(GPIO_BUZZER, HIGH);
+        delay(delayAmount);
+        digitalWrite(GPIO_BUZZER, LOW);
+}
+
+void setOnQuit(void (*function)(int))
+{
+        // set handler for cleanup
+	struct sigaction sigIntHandler;
+
+	sigIntHandler.sa_handler = function;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+
+	sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
+void cleanup(int value)
+{
+	digitalWrite(GPIO_BUZZER, LOW);
+	digitalWrite(GPIO_TRIGGER, LOW);
+	exit(0);
 }
