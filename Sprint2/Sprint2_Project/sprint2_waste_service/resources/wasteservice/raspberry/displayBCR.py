3091e107ac758e1c6a5ac2db992daf4f35842473
Michele Righi <righi.michy@gmail.com>
2023-03-03 02:32:06 +0100

    Create displayBCR.py

---
+++ Sprint2/Sprint2_Project/sprint2_waste_service/resources/wasteservice/raspberry/displayBCR.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Usage: ./displayBCR.py <text-for-line-1> <text-for-line-2> 
+
+from rpi_lcd import LCD
+import sys
+
+lcd = LCD()
+
+if len(sys.argv) == 1 or len(sys.argv) > 3:
+        lcd.clear()
+        exit(0)
+
+for i in range(1, len(sys.argv)):
+        lcd.text(str(sys.argv[i]), i)
