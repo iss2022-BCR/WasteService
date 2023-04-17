1797874d51df70dbd4ef819ec879867b6db45b83
Michele Righi <righi.michy@gmail.com>
2023-04-17 05:22:34 +0200

    update test radarSystem22.domainBCR

--- Sprint2/Sprint2_Project/it.unibo.radarSystem22.domainBCR/test/it/unibo/radarSystem22/domainBCR/TestSonarMock.java
+++ Sprint2/Sprint2_Project/it.unibo.radarSystem22.domainBCR/test/it/unibo/radarSystem22/domainBCR/TestSonarMock.java
@@ -1,5 +1,7 @@
 package it.unibo.radarSystem22.domainBCR;
 import static org.junit.Assert.assertTrue;
+
+import it.unibo.radarSystem22.domainBCR.utils.ColorsOut;
 import org.junit.*;
 
 import it.unibo.radarSystem22.domainBCR.interfaces.ISonar;
@@ -18,7 +20,7 @@ public class TestSonarMock {
 		System.out.println("down");		
 	}	
 	
-	@Test 
+	//@Test
 	public void testSonarMock() {
 		DomainSystemConfig.simulation = true;
 		DomainSystemConfig.testing    = false;
@@ -30,6 +32,38 @@ public class TestSonarMock {
 		sonar.activate();		
  		while( sonar.isActive() ) { BasicUtils.delay(1000);} //to avoid premature exit
  	}
-	
- 
+
+	 @Test
+	public void testSonarMockCLI()
+	{
+		DomainSystemConfig.simulation = true;
+		DomainSystemConfig.testing    = false;
+		DomainSystemConfig.sonarDelay = 50;		//quite fast generation ...
+
+		int delta = 5;
+		ISonar sonar = DeviceFactory.createSonar();
+
+		new Thread() {
+			public void run() {
+				BasicUtils.delay(4000);
+				sonar.deactivate();
+			}
+		}.start();
+		sonar.activate();
+
+		int prev = DomainSystemConfig.sonarDistanceMax;
+		while(sonar.isActive()) {
+			int curr = sonar.getDistance().getVal();
+			if(curr < 1)
+				curr = prev = DomainSystemConfig.sonarDistanceMax;
+			if(curr != prev)
+			{
+				assertTrue(curr == prev - delta);
+				prev = curr;
+			}
+			else BasicUtils.delay(20);
+		}
+
+		BasicUtils.delay(2000);
+	}
 }
